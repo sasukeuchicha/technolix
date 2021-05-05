ee423a9ff05cc0ca0923caad43b32a059e4a4317
Ramesses ii <kjayanth807@gmail.com>
2021-05-05 14:24:23 +0530

    nav bar perfecto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,13 +7,13 @@ return(
     <div className="container-fluid nav_bg">
         <div className="row">
             <div className="col-10 mx-auto">
-            <nav className="navbar navbar-expand-lg navbar-light ">
+            <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
                     <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                         <NavLink className="nav-link" exact aria-current="page" to="/">Home</NavLink>
                         </li>
